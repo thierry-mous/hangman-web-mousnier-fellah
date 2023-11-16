ef603adbff37ffd5f9c40690bdd26e5ae2588dfc
thierry-mous <thierry.mousnier@ynov.com>
2023-11-16 09:23:31 +0100

    add file

---
+++ code.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"os"
+)
+
+func main() {
+	temp, err := template.ParseGlob("./template/*.html")
+	if err != nil {
+		fmt.Println(fmt.Sprintf("Erreur => %s", err.Error()))
+		return
+	}
+
+	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
+		temp.ExecuteTemplate(w, "home", nil)
+	})
+	http.HandleFunc("/rules", func(w http.ResponseWriter, r *http.Request) {
+		temp.ExecuteTemplate(w, "rules", nil)
+	})
+	http.HandleFunc("/level1", func(w http.ResponseWriter, r *http.Request) {
+		temp.ExecuteTemplate(w, "level1", nil)
+	})
+	http.HandleFunc("/level2", func(w http.ResponseWriter, r *http.Request) {
+		temp.ExecuteTemplate(w, "level2", nil)
+	})
+	http.HandleFunc("/level3", func(w http.ResponseWriter, r *http.Request) {
+		temp.ExecuteTemplate(w, "level3", nil)
+	})
+	http.HandleFunc("/level4", func(w http.ResponseWriter, r *http.Request) {
+		temp.ExecuteTemplate(w, "level4", nil)
+	})
+	http.HandleFunc("/level5", func(w http.ResponseWriter, r *http.Request) {
+		temp.ExecuteTemplate(w, "level5", nil)
+	})
+	http.HandleFunc("/level6", func(w http.ResponseWriter, r *http.Request) {
+		temp.ExecuteTemplate(w, "level6", nil)
+	})
+	rootDoc, _ := os.Getwd()
+	fileserver := http.FileServer(http.Dir(rootDoc + "/asset"))
+	http.Handle("/static/", http.StripPrefix("/static/", fileserver))
+
+	http.ListenAndServe("localhost:8085", nil)
+}

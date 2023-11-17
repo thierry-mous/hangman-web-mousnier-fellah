4e2d5c0c4ca9b16df6e71a2ad0bdf7882fe91338
ademlsgo <145021201+ademlsgo@users.noreply.github.com>
2023-11-17 12:26:44 +0100

    home commit

--- code.go
+++ code.go
@@ -8,38 +8,30 @@ import (
 )
 
 func main() { //lire tout nos templates html
-	temp, err := template.ParseGlob("./template/*.html")
-	if err != nil {
-		fmt.Println(fmt.Sprintf("Erreur => %s", err.Error()))
-		return
-	}
+	tmpl := template.Must(template.ParseFiles("template/home.html", "template/level.html", "template/end.html", "template/menu.html"))
 
 	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
-		temp.ExecuteTemplate(w, "home", nil)
+		tmpl.ExecuteTemplate(w, "home", nil)
 	})
-	
-	http.HandleFunc("/level1", func(w http.ResponseWriter, r *http.Request) {
-		temp.ExecuteTemplate(w, "level1", nil)
+
+	http.HandleFunc("/level", func(w http.ResponseWriter, r *http.Request) {
+		tmpl.ExecuteTemplate(w, "level", nil)
 	})
-	http.HandleFunc("/level2", func(w http.ResponseWriter, r *http.Request) {
-		temp.ExecuteTemplate(w, "level2", nil)
+
+	http.HandleFunc("/end", func(w http.ResponseWriter, r *http.Request) {
+		tmpl.ExecuteTemplate(w, "end", nil)
 	})
-	http.HandleFunc("/level3", func(w http.ResponseWriter, r *http.Request) {
-		temp.ExecuteTemplate(w, "level3", nil)
+
+	http.HandleFunc("/menu", func(w http.ResponseWriter, r *http.Request) {
+		tmpl.ExecuteTemplate(w, "menu", nil)
 	})
-	http.HandleFunc("/level4", func(w http.ResponseWriter, r *http.Request) {
-		temp.ExecuteTemplate(w, "level4", nil)
-	})
-	http.HandleFunc("/level5", func(w http.ResponseWriter, r *http.Request) {
-		temp.ExecuteTemplate(w, "level5", nil)
-	})
-	http.HandleFunc("/level6", func(w http.ResponseWriter, r *http.Request) {
-		temp.ExecuteTemplate(w, "level6", nil)
-	})
+
 	//lie le css
 	rootDoc, _ := os.Getwd()
+	fmt.Println("Serveur écoutant sur le port 8085")
 	fileserver := http.FileServer(http.Dir(rootDoc + "/asset"))
 	http.Handle("/static/", http.StripPrefix("/static/", fileserver))
 
 	http.ListenAndServe("localhost:8085", nil)
+
 }

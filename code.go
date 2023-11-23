ba687f3b88d96d63fa8f3804f47e6bc1787ece3f
thierry-mous <thierry.mousnier@ynov.com>
2023-11-23 21:46:29 +0100

    first commit

--- code.go
+++ code.go
@@ -1,45 +1,50 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"html/template"
+	"log"
+	"math/rand"
 	"net/http"
 	"os"
+	"time"
 )
+
+var Level string
+var file string
 
 func main() { //lire tout nos templates html
 
 	tmpl, err := template.ParseGlob("./template/*.html")
-    if err != nil {
-        fmt.Printf(fmt.Sprintf("ERREUR => %s", err.Error()))
-        return
-    }
+	if err != nil {
+		fmt.Printf(fmt.Sprintf("ERREUR => %s", err.Error()))
+		return
+	}
 
 	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
 		tmpl.ExecuteTemplate(w, "home", nil)
 	})
 
-	http.HandleFunc("/level", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/game", func(w http.ResponseWriter, r *http.Request) {
 		tmpl.ExecuteTemplate(w, "level", nil)
 	})
 
-	http.HandleFunc("/victory", func(w http.ResponseWriter, r *http.Request) {
-		tmpl.ExecuteTemplate(w, "victory", nil)
+	http.HandleFunc("/result", func(w http.ResponseWriter, r *http.Request) {
+		tmpl.ExecuteTemplate(w, "result", nil)
 	})
 
-	http.HandleFunc("/menu", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/start", func(w http.ResponseWriter, r *http.Request) {
 		tmpl.ExecuteTemplate(w, "menu", nil)
 
 	})
 
 	http.HandleFunc("/choice", func(w http.ResponseWriter, r *http.Request) {
-		menu := r.FormValue("level")
-		fmt.Println(menu)
-	})
 
-	http.HandleFunc("/defaite", func(w http.ResponseWriter, r *http.Request) {
-		tmpl.ExecuteTemplate(w, "defaite", nil)
-
+		Level = r.FormValue("level")
+		filename := Selection(Level)
+		readfile := getWordFromFile(filename)
+		fmt.Println(Level)
 	})
 
 	//lie le css
@@ -50,4 +55,51 @@ func main() { //lire tout nos templates html
 
 	http.ListenAndServe("localhost:8085", nil)
 
+}
+
+func Selection(Level string) string {
+	switch Level {
+	case "1":
+		return "level 1"
+	case "2":
+		return "level 2"
+	case "3":
+		return "level 3"
+	case "4":
+		return "level 4"
+	case "5":
+		return "level 5"
+	case "6":
+		return "level 6"
+	default:
+		return "level 1"
+
+	}
+}
+
+func getWordFromFile(filename string) string {
+	// filePath, _ := filepath.Abs("../KANTIN-FAGN-FAGNIART-MOUSNIER-HANGMAN/Listes/" + filename)
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	var words []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		words = append(words, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	rand.Seed(time.Now().UnixNano())
+	return words[rand.Intn(len(words))]
+}
+
+type Hangman struct {
+	currentWordState, guesses []string
+	maxGuesses, numOfTries    int
+	word                      string
+	verbose                   bool
 }
